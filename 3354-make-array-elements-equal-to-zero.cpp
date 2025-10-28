5c945afca017ed3bb7083d7865f7c2855fb7cb5f
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-10-28 19:33:46 -0400

    Add LeetCode 3354 Make Array Elements Equal to Zero

---
+++ 3354-make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,109 @@
+#include <numeric>
+
+// Include custom utilities header
+#include <utilities.h>
+
+// Use standard namespace
+using namespace std;
+
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int result = 0;
+
+        auto it = nums.begin();
+        while ((it = find(it, nums.end(), 0)) != nums.end()) {
+            int left_sum = accumulate(nums.begin(), it, 0);
+            int right_sum = accumulate(it, nums.end(), 0);
+            int diff = abs(left_sum - right_sum);
+
+            if(diff == 1) {
+                result++;
+            } else if(diff == 0) {
+                result += 2;
+            }
+
+            it++;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    printStartBanner("3354. Make Array Elements Equal to Zero", "O(n^2)", "O(1)");
+
+    // Initialize the solution
+    Solution s;
+
+    // Get the mode to run program in from user
+    string mode = selectMode();
+
+    if(isCustomMode(mode)) { // If custom mode, run with user input
+        customModeSelected();
+
+        // Initialize input string and loop
+        string input;
+        while(true) {
+            // Get nums input from user
+            cout << "Enter nums as a comma-separated string of integers: ";
+            getline(cin, input);
+            toLowercase(input);
+
+            // If exit or quit inputted, exit program
+            if(isExitMode(input)) {
+                exitModeSelected();
+                break;
+            } else if(isQuitMode(input)) {
+                return quitModeSelected();
+            }
+
+            // Parse nums input into nums vector
+            vector<int> nums = stringToIntVector(input);
+
+            // If no nums entered, skip
+            if(nums.empty()) {
+                cout << "INFO: No nums entered. Please try again. Skipping..." << endl;
+                continue;
+            }
+
+            // Calculate and print count of valid selections
+            int result = s.countValidSelections(nums);
+            cout << "Count of valid selections: " << result << endl;
+            cout << "----------------------------------------" << endl;
+        }
+    } else if(isDemoMode(mode)) { // If demo mode, run with demo data
+        demoModeSelected();
+
+        // Initialize 2D int vector demoNums
+        vector<vector<int>> demoNums = {
+            {1, 0, 2, 0, 3},
+            {2, 3, 4, 0, 4, 1, 0}
+        };
+
+        // Loop over sets of demo data, print them, then calculate count of valid selections
+        for(const auto& nums : demoNums) {
+            // Print demo nums
+            cout << "Running program with nums: [";
+            for(int j = 0; j < nums.size(); j++) {
+                cout << nums[j];
+                if(j < nums.size() - 1) {
+                    cout << ", ";
+                }
+            }
+            cout << "]" << endl;
+
+            // Calculate and print count of valid selections
+            int result = s.countValidSelections(const_cast<vector<int>&>(nums));
+            cout << "Count of valid selections: " << result << endl;
+            cout << "----------------------------------------" << endl;
+        }
+    } else if(isExitMode(mode) || isQuitMode(mode)) { // If exit or quit mode, exit program
+        return quitModeSelected();
+    } else { // Else, unknown mode, error
+        return unknownModeSelected(mode);
+    }
+
+    // Exit program successfully
+    return 0;
+}
